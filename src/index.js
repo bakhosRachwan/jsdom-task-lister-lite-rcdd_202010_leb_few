e7c6c2665c37ba0a8c07da3211da434a97ee43a9
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-22 08:04:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+ 
+document.getElementById("create-task-form").addEventListener('submit', function(e) {
+   let newTaskDesc = document.getElementById("new-task-description").value;
+   let taskList = document.getElementById("tasks");
+   
+    taskList.innerHTML += `<li> ${newTaskDesc} </li>`;
+    e.preventDeafault()
+    
+  });
 });
