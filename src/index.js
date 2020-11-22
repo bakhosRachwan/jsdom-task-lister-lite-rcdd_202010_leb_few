511fb41284702f7ce6f3d88b60364e3b4c7461e3
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-22 08:25:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   
  
-document.getElementById("submit").addEventListener('submit', function(event) {
+document.getElementById("create-task-form").addEventListener('submit', function(event) {
   event.preventDeafault()
    let newTaskDesc = document.getElementById("new-task-description").value;
    let taskList = document.getElementById("tasks");
